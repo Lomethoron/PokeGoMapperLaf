a5379d7cad6675865f7207690a87eaf0a2298278
Will Dixon <wdixon23192@gmail.com>
2016-12-31 15:21:54 -0500

    added Express and googlemaps api handler

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,10 @@
-var http = require('http');
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1234, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1234/');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(1234, function () {
+  console.log('Example app listening on port 1234!');
+});
