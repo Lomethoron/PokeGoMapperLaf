6c8e3cbe5a0a3254e5fb9c408798a1d279c2158c
Will Dixon <wdixon23192@gmail.com>
2016-12-31 19:25:44 -0500

    got the map to render

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-
+app.listen(1234, function () {
+	console.log('Example app listening on port 1234!');
+});
 
 
--- routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app, map){
-
-	app.get('/', function (req, res) {
-		res.render('index');
-	});
-
-}; //module.exports
