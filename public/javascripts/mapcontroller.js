1cd67dd890938b04f59b1d13f9a1d4170dedd250
Will Dixon <wdixon23192@gmail.com>
2017-10-13 17:55:51 -0400

    cleaned up logging in mapcontroller

--- public/javascripts/mapcontroller.js
+++ public/javascripts/mapcontroller.js
@@ -4,33 +4,32 @@ function initMap() {
 		lat : 40.6981811,
 		lng : -75.2089322
 	};
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom : 17,
-		center : laf
-	});
+    var map = new google.maps.Map(document.getElementById('map'), {
+        gestureHandling: "greedy"
+    });
 
 	var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(
 			40.6951129, -75.2136497), new google.maps.LatLng(40.7025829,
 			-75.2001077));
-
+    map.fitBounds(bounds);
 	google.maps.event
 			.addListener(map, 'dragend',
 					function() {
 						if (bounds.contains(map.getCenter()))
 							return;
-						console.log("Outside of Bounds");
+						//console.log("Outside of Bounds");
 						var curLat = map.getCenter().lat(), curLong = map
 								.getCenter().lng(), westBound = bounds
 								.getNorthEast().lng(), northBound = bounds
 								.getNorthEast().lat(), eastBound = bounds
 								.getSouthWest().lng(), southBound = bounds
 								.getSouthWest().lat();
-						console.log("CurLat: "+curLat);
-						console.log("CurLong: "+curLong);
-						console.log("North Bound: "+northBound);
-						console.log("East Bound: "+eastBound);
-						console.log("South Bound: "+southBound);
-						console.log("West: "+westBound);
+						//console.log("CurLat: "+curLat);
+						//console.log("CurLong: "+curLong);
+						//console.log("North Bound: "+northBound);
+						//console.log("East Bound: "+eastBound);
+						//console.log("South Bound: "+southBound);
+						//console.log("West: "+westBound);
 						if (curLat < southBound)
 							curLat = southBound;
 						if (curLat > northBound)
